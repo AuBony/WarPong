c75200c8cb4e689fe9e0a0299881d253c7cf04d9
AuBony <55869341+AuBony@users.noreply.github.com>
2022-03-23 16:02:15 +0100

    Class Warrior, Boss

---
+++ src/GameEngine/Boss.java
@@ -0,0 +1,14 @@
+package GameEngine;
+
+public class Boss {
+
+    double x;
+    double y;
+    int hp;
+
+    public Boss(double x, double y, int hp){
+        this.x = x;
+        this.y = y;
+        this.hp = hp;
+    }
+}
---
+++ src/GameEngine/Warrior.java
@@ -0,0 +1,15 @@
+package GameEngine;
+
+public class Warrior {
+
+    public double x;
+    public double y;
+    public int hp;
+
+    public Warrior(double x, double y, int hp) {
+        this.x = x;
+        this.y = y;
+        this.hp = hp;
+    }
+
+}
