1cfcc9fcac5bf6993aa5f2c75e7558ee7248b8f6
AuBony <55869341+AuBony@users.noreply.github.com>
2022-03-25 13:44:14 +0100

    Class Warrior, Boss

--- src/GameEngine/Boss.java
+++ src/GameEngine/Boss.java
@@ -1,10 +1,13 @@
 package GameEngine;
 
+import javafx.scene.image.Image;
+
 public class Boss {
 
     double x;
     double y;
     int hp;
+    Image[] headBoss = {new Image("Resources/Sprites/dragon_100x79.png")};
 
     public Boss(double x, double y, int hp){
         this.x = x;
--- src/Render.java
+++ src/Render.java
@@ -1,14 +1,42 @@
+import com.example.warpong.HelloApplication;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import java.io.IOException;
 
 public class Render extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
+    @Override
+    public void start(Stage stage) {
+//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
+        try{
+            int width = 1000;
+            int height = 600;
+
+            Group root = new Group();
+
+            Scene scene;
+            scene = new Scene(root, width, height);
+            scene.setFill(Color.BLACK);
+
+
+            stage.setScene(scene);
+            stage.setTitle("W A R  P O N G");
+
+            stage.show();
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
-    @Override
-    public void start(Stage primaryStage) {
+    public static void main(String[] args) {
 
+        launch(args);
     }
 }
