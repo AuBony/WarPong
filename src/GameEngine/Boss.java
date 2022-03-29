140caccdba827aaf98af2161d55075745b54b645
Iorgadmin <thomas.museur@gmail.com>
2022-03-29 11:59:57 +0200

    Modify Canvas + Boss set at the center + Fond defined in canvas

--- src/GameEngine/Boss.java
+++ src/GameEngine/Boss.java
@@ -5,9 +5,14 @@ import javafx.scene.image.Image;
 public class Boss extends Entities{
 
     int hp = 100;
+    int heightBoss = 70;
+    int widthBoss = 60;
 
-    public Boss(double x, double y, double height, double width){
-        super(x, y, height, width);
+    public Boss(double x, double y, double heightBoss, double widthBoss){
+        super(x, y, heightBoss, widthBoss);
+        this.x = x - widthBoss/2;
+        this.y = y - heightBoss/2;
+
     }
     Image headBoss = new Image("Resources/Sprites/dragon_100x79.png");
 
--- src/Render.java
+++ src/Render.java
@@ -6,6 +6,9 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.RadialGradient;
+import javafx.scene.paint.Stop;
 import javafx.stage.Stage;
 
 public class Render extends Application {
@@ -17,24 +20,50 @@ public class Render extends Application {
             Group root = new Group();
 
             //Create a scene
-            final int width = 1000;
-            final int height = 600;
+            final double width = 1000;
+            final double height = 600;
             Scene scene = new Scene(root, width, height);
-            //scene.setFill(Color.WHITE); //new RadialGradient(0, 0, 500, 300, 500, false, CycleMethod.NO_CYCLE, new Stop(0, Color.grayRgb(60)), new Stop(1, Color.GREY))
+            //scene.setFill(Color.WHITE);
+            // new RadialGradient(0, 0, 500, 300, 500, false, CycleMethod.NO_CYCLE, new Stop(0, Color.grayRgb(60)), new Stop(1, Color.GREY));
 
 
             //Create a canvas
             Canvas canvas = new Canvas(width, height);
             root.getChildren().add(canvas);
             GraphicsContext gc = canvas.getGraphicsContext2D();
+            gc.setFill(Color.BLACK);
+
+            //Add fond
+            Image fond = new Image ("Resources/Sprites/Fond.png");
+            gc.drawImage(fond, 50, 50, width-100, height-100);
+
+
             //gc.setFill(Color.WHITE);
 
             //Add Boss
-            Boss boss = new Boss(width/2, height/2, 100, 79);
+            Boss boss = new Boss((width/2),(height/2), 70, 60);
             Image ImBoss = boss.getHeadBoss();
             gc.drawImage(ImBoss, boss.getX(), boss.getY(), boss.getWidth(), boss.getHeight());
 
             stage.setScene(scene); //Add scene to stage
+
+            //Timeloop
+/*            final long startNanoTime = System.nanoTime();
+            new AnimationTimer()
+            {
+                    public void handle(long currentNanoTime)
+                    {
+                            double t = (currentNanoTime -
+                                    startNanoTime) / 1000000000.0;
+                            double x = 232 + 128 * Math.cos(t);
+                            double y = 232 + 128 * Math.sin(t);
+                            gc.drawImage( space, 0, 0 );
+                            gc.drawImage( earth, x, y );
+                            gc.drawImage( sun, 196, 196 );
+                    }
+            }.start();
+            /*
+ */
 
             //Stage
             stage.setTitle("W A R  P O N G");
