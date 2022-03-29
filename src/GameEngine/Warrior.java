8868bcee6e9d877ad3dd4afd8fc4f06fd203ba31
AuBony <55869341+AuBony@users.noreply.github.com>
2022-03-29 13:46:27 +0200

    Add new skin

--- src/GameEngine/Warrior.java
+++ src/GameEngine/Warrior.java
@@ -14,10 +14,14 @@ public class Warrior extends Entities{
     public Warrior(double x, double y, double height, double width, String type){
         super(x, y, height, width);
         if(type.equals("joueur 1")){
-            skinWarrior = new Image("Resources/Sprites/skin1.png");
+            skinWarrior = new Image("Resources/Sprites/dragon_100x79.png");
+            this.setHeightEntities(158);
+            this.setWidthEntities(135);
         }
         else if(type.equals("joueur 2")){
-            skinWarrior = new Image("Resources/Sprites/skin1.png");
+            skinWarrior = new Image("Resources/Sprites/Audreya.png");
+            this.setHeightEntities(169);
+            this.setWidthEntities(125);
         }else{
             skinWarrior = new Image("Resources/Sprites/dragon_100x79.png");
             this.setHeightEntities(100);
